20240006fafd57d99ab4388a81818eda6eaca159
Aleksandr <a0933093518@gmail.com>
2021-11-13 22:06:21 +0200

    rout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 
 import { Route, Routes } from "react-router-dom";
-// import { useState } from "react";
-// import s from "./App.module.css";
+import { useState } from "react";
+import s from "./App.module.css";
 // import Modal from "../Modal";
-// import BackgroundAuth from "../BackgroundAuth/BackgroundAuth";
+import BackgroundAuth from "../BackgroundAuth/BackgroundAuth";
 import Header from "../Header/Header";
 import RegisterForm from "../RegisterForm";
+import LoginForm from "../LoginForm";
 import ProjectList from "../ProjectList";
 import Container from "../Container/Container";
 
@@ -14,17 +15,20 @@ function App() {
   return (
     <>
 
-      <Header />
-      {/* <div className={s.container}> */}
-      {/* <BackgroundAuth /> */}
+<Header />
+     <Routes>
+      <Route path="/" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    <div className={s.container}>
+      <BackgroundAuth />
+    </div>
       <Container>
         <ProjectList />
       </Container>
-      {/* <RegisterForm /> */}
-      <Routes>
-        <Route path="/signup" component={RegisterForm} />
-      </Routes>
-      {/* </div> */}
+      
+     
+      
 
     </>
   );
