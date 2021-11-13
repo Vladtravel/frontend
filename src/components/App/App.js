5ee5bce974f8435259ba8b59b6490ece551a5a03
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-11-13 21:16:56 +0200

    Сделал ProjectItem разметку и css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import s from "./App.module.css";
-import Modal from "../Modal";
-import BackgroundAuth from "../BackgroundAuth/BackgroundAuth";
+// import { useState } from "react";
+// import s from "./App.module.css";
+// import Modal from "../Modal";
+// import BackgroundAuth from "../BackgroundAuth/BackgroundAuth";
 import Header from "../Header/Header";
 import RegisterForm from "../RegisterForm";
+import ProjectList from "../ProjectList";
+import Container from "../Container/Container";
 
 function App() {
   return (
     <>
-    <Header />
-    <div className={s.container}>
-      <BackgroundAuth />
-      
+      <Header />
+      {/* <div className={s.container}> */}
+      {/* <BackgroundAuth /> */}
+      <Container>
+        <ProjectList />
+      </Container>
       {/* <RegisterForm /> */}
       <Routes>
         <Route path="/signup" component={RegisterForm} />
       </Routes>
-    </div>
+      {/* </div> */}
     </>
   );
 }
