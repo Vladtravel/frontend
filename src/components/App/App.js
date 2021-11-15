9ea094e296efdd26ac1599d17e3684c069104b39
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-11-15 20:10:31 +0200

    Добавил  ховеры

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,46 +1,32 @@
-
 import { Route, Routes } from "react-router-dom";
-// import { useState } from "react";
 import s from "./App.module.css";
-// import Modal from "../Modal";
 import BackgroundAuth from "../BackgroundAuth/BackgroundAuth";
 import Header from "../Header/Header";
 import RegisterForm from "../RegisterForm";
 import LoginForm from "../LoginForm";
 import ProjectList from "../ProjectList";
 import Container from "../Container/Container";
-import SingleSprint from "../Sprint/SingleSprint"
-
+import SingleSprint from "../Sprint/SingleSprint";
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<RegisterForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route
-          path="/projects"
-          element={
-            <Container>
-              <ProjectList />
-            </Container>
-          }
-        />
-        <Route path="/projects/:projectId" element={<h1>Hello World</h1>} />
-
-      </Routes>
-    <div className={s.container}>
-      <BackgroundAuth />
-    </div>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/projects" element={<ProjectList />} />
+          <Route path="/projects/:projectId" element={<SingleSprint />} />
+        </Routes>
+      </Container>
+      {/* <div className={s.container}>
+        <BackgroundAuth />
+      </div>
       <Container>
         <ProjectList />
-        < SingleSprint/>
-      </Container>
-      
-     
-      
-
+        <SingleSprint />
+      </Container> */}
     </>
   );
 }
