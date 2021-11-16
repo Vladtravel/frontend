f43c7daae8bc6eee6019079c76483063eae1b08a
Aleksandr <a0933093518@gmail.com>
2021-11-16 11:02:10 +0200

    reg

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Navigate } from "react-router-dom";
+import {  Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import selectors from "../redux/selectors";
 
@@ -7,21 +7,27 @@ import selectors from "../redux/selectors";
  * - Если маршрут приватный и пользователь залогинен, рендерит компонент
  * - В противном случае рендерит Redirect на /login
  */
-const PrivateRoute = ({ component: Component, redirectTo, ...routeProps }) => {
-  const isAuthenticated = useSelector(selectors.isAuthenticated);
+// const PrivateRoute = ({ component: Component, redirectTo, ...routeProps }) => {
+//   const isAuthenticated = useSelector(selectors.isAuthenticated);
 
-  return (
-    <Route
-      {...routeProps}
-      render={(props) =>
-        isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Navigate to={redirectTo} />
-        )
-      }
-    />
-  );
-};
+//   return (
+//     <Route
+//       {...routeProps}
+//       render={(props) =>
+//         isAuthenticated ? (
+//           <Component {...props} />
+//         ) : (
+//           <Navigate to={redirectTo} />
+//         )
+//       }
+//     />
+//   );
+// };
 
+// export default PrivateRoute;
+
+function PrivateRoute({ children, redirectTo }) {
+  let isAuthenticated = useSelector(selectors.isAuthenticated);
+  return isAuthenticated ? children : <Navigate to={redirectTo} />;
+}
 export default PrivateRoute;
