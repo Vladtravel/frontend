c2bd40e5d866bee81d0a2d82da7773ec92753262
Vladyslav <vladtravel2015@gmail.com>
2021-11-16 20:25:43 +0200

    тест

---
+++ src/components/App/App.js
@@ -0,0 +1,49 @@
+import { Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import Header from "../Header/Header";
+import RegisterForm from "../RegisterForm";
+import LoginForm from "../LoginForm";
+import ProjectList from "../ProjectList";
+import Container from "../Container/Container";
+import operations from "../../redux/operations";
+import ConfirmView from "../ConfirmView";
+// import SingleSprint from "../Sprint/SingleSprint"
+import PrivateRoute from "../PrivateRoute";
+import PublicRoute from "../PublicRoute";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(operations.getCurrentUser());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={RegisterForm} />
+        <PublicRoute path="/login" component={LoginForm} />
+        <Route path="/confirmation" component={ConfirmView} />
+        <Container>
+          <Route path="/projects" component={ProjectList} />
+        </Container>
+        <PublicRoute
+          path="/login"
+          component={LoginForm}
+          restricted
+          redirectTo="/projects"
+        />
+
+        <PrivateRoute
+          path="/projects"
+          component={ProjectList}
+          redirectTo="/login"
+        />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
