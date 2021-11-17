c3cea12390bd9d4279fa012f7e88f0835fdf1f08
Aleksandr <a0933093518@gmail.com>
2021-11-18 00:46:46 +0200

    menu sprimts scelet

--- src/components/ProjectSprints/ProjectSprints.js
+++ src/components/ProjectSprints/ProjectSprints.js
@@ -8,10 +8,32 @@ function ProjectSprints(){
 return(
     <div className={s.Sprints}>
     <div className={s.menuSprints}>
-            <span className={s.iconLink}></span>
-            <NavLink to="/projects" className={s.menuLink}>
-                Показати проекти 
-            </NavLink>
+            <div className={s.menuNav}>
+                <span className={s.iconLink}></span>
+                <NavLink to="/projects" className={s.menuLink}>
+                    Показати <br></br>проекти 
+                </NavLink>
+            </div>
+            <div className={s.menuProjects}>
+                <ul>
+                <li><NavLink to="/projects/1" className={s.menuLink}>
+                    Проект 1
+                    </NavLink>
+                </li>
+                <li><NavLink to="/projects/2" className={s.menuLink}>
+                    Проект 2
+                    </NavLink>
+                </li>
+                <li><NavLink to="/projects/3" className={s.menuLink}>
+                    Проект 3
+                    </NavLink>
+                </li>
+                 
+                </ul>
+             </div>
+            <div className={s.menuAdd}>
+                <samp>Створити проект</samp>
+            </div>
     </div>
     <div>
         <SingleSprint/> 
