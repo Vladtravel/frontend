52cb09d68c8cb5e8218a1ff9b81404af810a2a2f
Aleksandr <a0933093518@gmail.com>
2021-11-17 13:45:40 +0200

    route востановление

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,23 +1,22 @@
-import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import selectors from "../redux/selectors";
 
-const PublicRoute = ({ component: Component, redirectTo, ...routeProps }) => {
-  const isAuthenticated = useSelector(selectors.isAuthenticated);
- 
-  return (
-    <Route
-      {...routeProps}
-      render={(props) =>
-        isAuthenticated && routeProps.restricted ? (
-          <Redirect to={redirectTo} />
-        ) : (
-          <Component {...props} />
-        )
-      }
-    />
-  );
-};
+export default function PublicRouter({
+  children,
+  redirectTo = "/projects",
+  restricted = false,
+  ...routeProps
+}) {
+  const isLoggedIn = useSelector(selectors.isAuthenticated);
 
-export default PublicRoute;
+  const shouldRedirect = isLoggedIn && restricted;
+
+  return (
+    <Route {...routeProps}>
+      {shouldRedirect ? <Redirect to={redirectTo} /> : children}
+    </Route>
+
+
+  );
+}
