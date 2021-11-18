474cd8d8c1dfc207389b4914d7c43f671cefd32a
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2021-11-18 21:59:30 +0200

    commit

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,6 +1,8 @@
 import ProjectItem from "../ProjectItem";
 import ProjectButtonAdd from "./ProjectButtonAdd/ProjectButtonAdd";
 
+import Add from "../MemberForm/MemberForm"
+
 const ProjectList = () => {
   return (
     <>
