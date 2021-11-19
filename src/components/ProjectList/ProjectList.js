95c1c24197838d019904cf1bbf2f6e6dfdc9697f
Vladyslav <vladtravel2015@gmail.com>
2021-11-19 11:27:53 +0200

    притиер

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,16 +1,12 @@
 import ProjectItem from "../ProjectItem";
 import ProjectButtonAdd from "./ProjectButtonAdd/ProjectButtonAdd";
 
-import Add from "../MemberForm/MemberForm"
+import Add from "../MemberForm/MemberForm";
 
 const ProjectList = () => {
   return (
     <>
-      <ProjectButtonAdd
-        text={"Проекти"}
-        description={" Створити проект"}
-        className={"container"}
-      />
+      <ProjectButtonAdd text={"Проекти"} description={" Створити проект"} className={"container"} />
 
       <ProjectItem />
     </>
--- src/components/ProjectSprints/ProjectSprints.js
+++ src/components/ProjectSprints/ProjectSprints.js
@@ -1,71 +1,66 @@
 import { NavLink } from "react-router-dom";
 import SingleSprint from "../Sprint/SingleSprint";
-import s from "./ProjectSprints.module.css"
+import s from "./ProjectSprints.module.css";
 import { useSelector } from "react-redux";
-import {getAllProjects} from "../../redux/projects/projects-selectors"
-import ProjectButtonAdd from "../ProjectList/ProjectButtonAdd"
+import { getAllProjects } from "../../redux/projects/projects-selectors";
+import ProjectButtonAdd from "../ProjectList/ProjectButtonAdd";
 import RenameSprint from "../Sprint/RenameSprint";
 
-import AddMember from "../MemberForm/MemberForm"
+import AddMember from "../MemberForm/MemberForm";
 
 const randomColor = [
-    "rgba(255, 107, 8)",
-    "rgba(140, 114, 223)",
-    "rgba(113, 223, 129)",
-    "rgba(60, 114, 223)",
-    "rgba(113, 191, 231)",
-  ];
-  
-function ProjectSprints(){
-    const getProjects = useSelector(getAllProjects);
-    
-return(
+  "rgba(255, 107, 8)",
+  "rgba(140, 114, 223)",
+  "rgba(113, 223, 129)",
+  "rgba(60, 114, 223)",
+  "rgba(113, 191, 231)",
+];
+
+function ProjectSprints() {
+  const getProjects = useSelector(getAllProjects);
+
+  return (
     <div className={s.Sprints}>
-    <div className={s.menuSprints}>
-            <div className={s.menuNav}>
-                <div className={s.iconLink}></div>
-                <NavLink to="/projects" className={s.menuLink}>
-                    Показати <br></br>проекти 
-                </NavLink>
-            </div>
-            <div className={s.menuProjects}>
-            <ul className={s.item}>
-      {getProjects.map(({ name, _id }) => {
-        const color =
-          randomColor[Math.floor(Math.random() * randomColor.length)];
-        return (
-          <li
-            key={_id}
-            className={s.menuProjectLink}
-          >
-            <div 
-            className={s.menuProjectIcon}
-            style={{
-              backgroundColor: color,
-              boxShadow: `0px 3px 4px ${color}`,
-            }}></div>
-            <NavLink to={`/projects/${_id}/sprints`} className={s.link}>
-              <h3 className={s.subtitle}>{name}</h3>
-            </NavLink>
-        
-          </li>
-        );
-      })}
-    </ul>
-             </div>
-            <div className={s.menuAdd}>
-                <ProjectButtonAdd className={"projectSprintsAdd"}/>
-                <samp>Створити проект</samp>
-            </div>
-    </div>
-    <div>
-        <RenameSprint/>
+      <div className={s.menuSprints}>
+        <div className={s.menuNav}>
+          <div className={s.iconLink}></div>
+          <NavLink to="/projects" className={s.menuLink}>
+            Показати <br></br>проекти
+          </NavLink>
+        </div>
+        <div className={s.menuProjects}>
+          <ul className={s.item}>
+            {getProjects.map(({ name, _id }) => {
+              const color = randomColor[Math.floor(Math.random() * randomColor.length)];
+              return (
+                <li key={_id} className={s.menuProjectLink}>
+                  <div
+                    className={s.menuProjectIcon}
+                    style={{
+                      backgroundColor: color,
+                      boxShadow: `0px 3px 4px ${color}`,
+                    }}
+                  ></div>
+                  <NavLink to={`/projects/${_id}/sprints`} className={s.link}>
+                    <h3 className={s.subtitle}>{name}</h3>
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className={s.menuAdd}>
+          <ProjectButtonAdd className={"projectSprintsAdd"} />
+          <samp>Створити проект</samp>
+        </div>
+      </div>
+      <div>
+        <RenameSprint />
         {/* Сдесь делай кнопку */}
-        < AddMember/>
-        <SingleSprint/> 
+        <AddMember />
+        <SingleSprint />
+      </div>
     </div>
-    
-    </div>
-)
+  );
 }
 export default ProjectSprints;
